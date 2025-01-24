7e9ed17ffd74881bd140cc18ca3b5b50b3bf8fe2
Alex Monzon <alexmonzon@MacBook-Pro-de-Alex.local>
2025-01-24 20:52:38 +0100

    Initial commit

---
+++ src/main/java/com/airlines/app_wings/AppWingsApplication.java
@@ -0,0 +1,13 @@
+package com.airlines.app_wings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airlines/app_wings/AppWingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlines.app_wings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
