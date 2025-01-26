ec664ce28ba2ac178e03cc0db150e636c8427261
Alex Monzon <alexmonzon@MacBook-Pro-de-Alex.local>
2025-01-26 18:44:15 +0100

    add user, profile, roles entityes + h2 config

---
+++ src/main/java/com/airlines/app_wings/roles/Role.java
@@ -0,0 +1,31 @@
+package com.airlines.app_wings.roles;
+
+import com.airlines.app_wings.users.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String role;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    Set<User> users;
+
+}
