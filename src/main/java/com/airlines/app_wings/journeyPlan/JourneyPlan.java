e67a4c27d092c1e12c63ea1291562596a503b926
Alex Monzon <alexmonzon@MacBook-Pro-de-Alex.local>
2025-01-27 13:37:17 +0100

    Add Journey Plan controller and service

--- src/main/java/com/airlines/app_wings/journeyPlan/JourneyPlan.java
+++ src/main/java/com/airlines/app_wings/journeyPlan/JourneyPlan.java
@@ -21,8 +21,11 @@ public class JourneyPlan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalTime time;
-    private LocalDate date;
+    private LocalTime localDepartureTime;
+    private LocalDate localDeparturelDate;
+
+    private LocalTime localArrivalTime;
+    private LocalDate localArrivalDate;
 
     @ManyToOne
     @JoinColumn(name = "id_origin", nullable = false)
@@ -32,5 +35,59 @@ public class JourneyPlan {
     @JoinColumn(name = "id_destini", nullable = false)
     private Destiny destiny;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalTime getLocaldepartureTime() {
+        return localDepartureTime;
+    }
+
+    public void setLocaldepartureTime(LocalTime localdepartureTime) {
+        this.localDepartureTime = localdepartureTime;
+    }
+
+    public LocalDate getLocaldeparturelDate() {
+        return localDeparturelDate;
+    }
+
+    public void setLocaldeparturelDate(LocalDate localdeparturelDate) {
+        this.localDeparturelDate = localdeparturelDate;
+    }
+
+    public LocalTime getLocalArrivalTime() {
+        return localArrivalTime;
+    }
+
+    public void setLocalArrivalTime(LocalTime localArrivalTime) {
+        this.localArrivalTime = localArrivalTime;
+    }
+
+    public LocalDate getLocalArrivalDate() {
+        return localArrivalDate;
+    }
+
+    public void setLocalArrivalDate(LocalDate localArrivalDate) {
+        this.localArrivalDate = localArrivalDate;
+    }
+
+    public Origin getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Origin origin) {
+        this.origin = origin;
+    }
+
+    public Destiny getDestiny() {
+        return destiny;
+    }
+
+    public void setDestiny(Destiny destiny) {
+        this.destiny = destiny;
+    }
 }
--- src/main/java/com/airlines/app_wings/origin/Origin.java
+++ src/main/java/com/airlines/app_wings/origin/Origin.java
@@ -26,4 +26,35 @@ public class Origin {
     private String codCity;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAirport() {
+        return airport;
+    }
+
+    public void setAirport(String airport) {
+        this.airport = airport;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCodCity() {
+        return codCity;
+    }
+
+    public void setCodCity(String codCity) {
+        this.codCity = codCity;
+    }
 }
